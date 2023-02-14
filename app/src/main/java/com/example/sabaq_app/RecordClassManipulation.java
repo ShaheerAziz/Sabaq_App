b334511cabfadac1f806a43ca74e6f0c010b18cd
ShaheerAziz <h.m.shaheeraziz@gmail.com>
2023-02-15 00:19:51 +0500

    Commit 4

---
+++ app/src/main/java/com/example/sabaq_app/RecordClassManipulation.java
@@ -0,0 +1,74 @@
+package com.example.sabaq_app;
+
+
+
+public class RecordClassManipulation {
+
+    // Date datetime;
+    String Name;
+    String Sabaq;
+    String Sabqi;
+    String Manzil;
+    //boolean isCorrect;
+
+
+    public RecordClassManipulation(String pName,    String pSabaq,    String pSabqi,    String pManzil) {
+
+        //  this.datetime = datetime;
+        Name= pName;
+        Sabaq=pSabaq;
+        Sabqi=pSabqi;
+        Manzil=pManzil;
+
+    }
+
+
+    public RecordClassManipulation() {
+
+        //  this.datetime = datetime;
+        Name= "";
+        Sabaq="";
+        Sabqi="";
+        Manzil="";
+
+    }
+
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getSabaq() {
+        return Sabaq;
+    }
+
+    public void setSabaq(String sabaq) {
+        Sabaq = sabaq;
+    }
+
+    public String getSabqi() {
+        return Sabqi;
+    }
+
+    public void setSabqi(String sabqi) {
+        Sabqi = sabqi;
+    }
+
+    public String getManzil() {
+        return Manzil;
+    }
+
+    public void setManzil(String manzil) {
+        Manzil = manzil;
+    }
+
+
+    @Override
+    public String toString() {
+        return Name ;
+    }
+}
